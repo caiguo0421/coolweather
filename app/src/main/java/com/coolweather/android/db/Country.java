2cbf4a80e004ec2d3792681e7f163bfe812c65ae
unknown <>
2017-02-14 17:03:35 +0800

    add db entry for contry city and province

---
+++ app/src/main/java/com/coolweather/android/db/Country.java
@@ -0,0 +1,52 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by caiguoxin on 2017/2/14.
+ * 街道
+ */
+
+public class Country extends DataSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private String weatherId;
+
+    private int cityId;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
